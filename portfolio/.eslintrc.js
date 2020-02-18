b144694a09b11dea16c83e10895dcddde3c34073
Jon Taylor <jonnytest1101@icloud.com>
2020-02-18 08:34:38 -0600

    Switch Frameworks
    Moved from strictly vue over to Quasar.
    
    Removed UIkit all together and any animations will be created using GreenSock "GSAP" and basic Quasar/Material Styling Library.

--- portfolio/.eslintrc.js
+++ portfolio/.eslintrc.js
@@ -1,28 +1,41 @@
 module.exports = {
   root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
+
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
+    sourceType: 'module'
   },
+
+  env: {
+    browser: true
+  },
+
+  extends: [
+    'prettier',
+    // https://eslint.vuejs.org/rules/#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/essential'
+  ],
+
+  // required to lint *.vue files
+  plugins: [
+    'vue'
+  ],
+
+  globals: {
+    'ga': true, // Google Analytics
+    'cordova': true,
+    '__statics': true,
+    'process': true,
+    'Capacitor': true,
+    'chrome': true
+  },
+
+  // add your custom rules here
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prefer-promise-reject-errors': 'off',
+
+    // allow debugger during development only
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
+  }
 }
---
+++ portfolio/src/pages/Index.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-page class="flex flex-center">
+    <img ref="logo" alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+  </q-page>
+</template>
+
+<script>
+import { TimelineLite, Back } from 'gsap';
+
+export default {
+  name: 'PageIndex',
+  mounted() {
+    const { logo } = this.$refs;
+    const timeLine = new TimelineLite();
+
+    timeLine
+    .to(logo, 3, {
+      x: 200,
+      rotation: 90,
+      ease: Back.easeInOut
+    })
+    .to(logo, 3, {
+      x: 0,
+      rotation: 0,
+      ease: Back.easeInOut
+    })
+  }
+}
+</script>
